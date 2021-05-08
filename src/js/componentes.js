6a3bacdfd04eb4af18f417ea3d0e518b04109c63
Francisco <francisco.pallero.f@gmail.com>
2021-05-08 19:18:06 -0400

    Se agrega funcionalidad para esconder footer cuando no existan registros

--- src/js/componentes.js
+++ src/js/componentes.js
@@ -2,34 +2,35 @@ import { Todo } from '../classes';
 import { todoList } from '../index'
 
 const divTodoList = document.querySelector('.todo-list');
+const footer = document.querySelector('.footer');
 const btnBorrar = document.querySelector('.clear-completed');
 const txtInput = document.querySelector('.new-todo');
 const ulFiltros = document.querySelector('.filters');
 const anchorFiltros = document.querySelectorAll('.filtro');
+const contador = document.querySelector('.todo-count');
+footer.setAttribute('hidden', '');
 
 export const crearTodoHtml = (todo) => {
+    footer.removeAttribute('hidden');
     const htmlTodo = `
-    <li class="${(todo.completado) ? 'completed' : ''}" data-id="${todo.id}">
-        <div class="view">
-            <input class="toggle" type="checkbox" ${(todo.completado) ? 'checked' : ''}>
-            <label>${todo.tarea}</label>
-            <button class="destroy"></button>
-        </div>
-        <input class="edit" value="Create a TodoMVC template">
-    </li>
-    `;
+            <li class="${(todo.completado) ? 'completed' : ''}" data-id="${todo.id}">
+                <div class="view">
+                    <input class="toggle" type="checkbox" ${(todo.completado) ? 'checked' : ''}>
+                    <label>${todo.tarea}</label>
+                    <button class="destroy"></button>
+                </div>
+                <input class="edit" value="Create a TodoMVC template">
+            </li>
+            `;
     const div = document.createElement('div');
-
     div.innerHTML = htmlTodo;
     divTodoList.append(div.firstElementChild);
-
     return div.firstElementChild;
 }
 
 txtInput.addEventListener('keyup', (event) => {
     if (event.keyCode === 13 && txtInput.value.trim().length > 0) {
         const nuevoTodo = new Todo(txtInput.value);
-
         todoList.nuevoTodo(nuevoTodo);
         crearTodoHtml(nuevoTodo);
         txtInput.value = '';
@@ -47,26 +48,26 @@ divTodoList.addEventListener('click', (event) => {
     } else if (nombreElemento.includes('button')) {
         todoList.eliminarTodo(todoId);
         divTodoList.removeChild(todoElemento);
+        if (divTodoList.children.length === 0) footer.setAttribute('hidden', '')
     }
 });
 
 btnBorrar.addEventListener('click', () => {
     for (let i = divTodoList.children.length - 1; i >= 0; i--) {
         const elemento = divTodoList.children[i];
-
         if (elemento.classList.contains('completed')) {
             divTodoList.removeChild(elemento);
         }
     }
+
+    if (divTodoList.children.length === 0) footer.setAttribute('hidden', '')
     todoList.eliminarCompletados();
 });
 
 ulFiltros.addEventListener('click', (event) => {
     const filtro = event.target.text;
     if (!filtro) { return; };
-
     anchorFiltros.forEach(elem => elem.classList.remove('selected'));
-
     for (const elemento of divTodoList.children) {
         elemento.classList.remove('hidden');
         const completado = elemento.classList.contains('completed');
@@ -84,7 +85,7 @@ ulFiltros.addEventListener('click', (event) => {
                 break;
         }
     }
-
 });
+
 
 // TODO: Agregar conteo de pendientes.
